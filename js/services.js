1a305a8b31af56800f5167aed0c784ef67c82e6d
fabio931217 <Lkto@MacBook-Air-de-Ember.local>
2016-05-02 01:40:59 -0500

    vbb

--- js/services.js
+++ js/services.js
@@ -7,7 +7,7 @@ angular.module('starter.services', [])
     "senderID": "217743739524"
   };
 
-  document.addEventListener("deviceready", function(){
+  //document.addEventListener("deviceready", function(){
 
     $cordovaPush.register(androidConfig).then(function(result) {
       // Success
@@ -48,7 +48,7 @@ angular.module('starter.services', [])
       // Error
     })*/
 
- }, false);
+ //}, false);
 })
 .constant('ENV', {name:'development',youtubeKey:'AIzaSyDael5MmCQa1GKQNKQYypmBeB08GATgSEo',ionicPrivateKey:'a9265eaf15a20cc8516c770e8748aeed4891b28f453ce755',ionicPublicKey:'e30d4d540b8c75d1f167bbf242423c3fb23fe10275d1c016',ionicAppId:'241b6d37',gcmId:'228071472080',instagramAppId:'2998ca20ed924ca3be22907c6ae77363',facebookPermanentAccessToken:'CAANL6xXrSHYBANNHhMUDugVZBHXfVQBMeWG6FmpYROWcOEmC2xze1BNiraZB87NCAZC3w08L7KhCBnhJItZCUzWCgBNzBjt0BkoV6qMoXjIZBjkWRTUGgZBR39OZAiP3DF76jufQ4hJ7xsdQc0l68vFAZAePdZCZAjkjTwaOeEZC22xi8ZAQYBqvNvYRgIfOZBzf4zRURHgrLtNazxzln8ZBkd9FZC7',firebaseUrl:'music-band-ionic.firebaseio.com',parse:{applicationId:'sidmrbO9OqG3pe4iErva408MHFysJZ2zChYPAXlU',key:'e49Rnlja6llKnFW5p0OOF8dkHvJi5o1hrVzFGBnc'},settingsSource:'LOCAL'})
 
